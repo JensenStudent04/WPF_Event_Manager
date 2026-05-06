cd55889b0cfd603d9472b71ad8346bed648146ef
JensenStudent04 <ST10442661@myemeris.edu.za>
2026-05-06 17:00:28 +0200

    Commit 4

--- WPF_Event_Manager/WPF_Event_Manager/MainWindow.xaml.cs
+++ WPF_Event_Manager/WPF_Event_Manager/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace WPF_Event_Manager
     public delegate bool RegistrationCriteria(Registration registration);
     public partial class MainWindow : Window
     {
+        RegistrationManager<Registration> manager = new RegistrationManager<Registration>();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,31 +29,91 @@ namespace WPF_Event_Manager
             // Create a Instance of Registration
             Registration registration = new Registration();
 
+            registration.EmployeeName = txtEmployeeName.Text;
+            registration.EventName = txtEventName.Text;
+            registration.Department = txtDepartment.Text;
+
+            if (!double.TryParse(txtFee.Text, out double fee))
+            {
+                MessageBox.Show("Please Enter a Valid Fee",
+                    "Fee Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+
+            if (!int.TryParse(txtDaysAttending.Text, out int daysAttending))
+            {
+                MessageBox.Show("Please Enter a Valid Number of Days",
+                    "Days Attending Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+
+            registration.Fee = fee;
+            registration.DaysAttending = daysAttending;
+
+            manager.AddRegistration(registration);
+
+            MessageBox.Show("Registration has been successfully Completed!");
+
+
         }
 
         private void btnFilterDepartment_Click(object sender, RoutedEventArgs e)
         {
+            txtOutput.Clear();
+
+            DisplayHeading("FILTER BY DEPARTMENT");
+
+            string selectedDepartment = txtFilterDepartment.Text;
+
+            txtOutput.AppendText(manager.DisplayMatchingRegistrations(delegate (Registration r)
+            {
+                return r.Department.ToUpper() == selectedDepartment.ToUpper();
+            }));
+
+
 
         }
 
         private void btnDisplayAll_Click(object sender, RoutedEventArgs e)
         {
+            txtOutput.Clear();
+
+            DisplayHeading("ALL REGISTRATIONS");
+            txtOutput.AppendText(manager.DisplayAllRegistrations());
 
         }
 
         private void btnNeedsApproval_Click(object sender, RoutedEventArgs e)
         {
+            txtOutput.Clear();
+
+            DisplayHeading("REGISTRATIONS REQUIRING APPROVAL");
+                txtOutput.AppendText(manager.DisplayMatchingRegistrations(delegate (Registration r) // Anonymous Method 
+                {
+                    return r.NeedsApproval();
+                }));
 
         }
 
         private void btnSummary_Click(object sender, RoutedEventArgs e)
         {
+            txtOutput.Clear();
 
+            DisplayHeading("SUMMARY");
+
+            txtOutput.AppendText("Total Fees Collected R" + manager.CalculateTotalFees() + "\n");
+
+            // FIX: Added AppendText right here!
+            txtOutput.AppendText("Total Registrations Requiring Approval: " +
+                manager.CountMatchingRegistrations(delegate (Registration r)
+                {
+                    return r.NeedsApproval();
+                }));
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-
+            ClearTextFields();
         }
 
         public void DisplayHeading(string heading)
